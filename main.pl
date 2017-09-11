470b1a6eaee632e727cd7b663189bc458a26b2b9
glm3531 <glm3531@gmail.com>
2017-09-11 13:26:22 -0600

    modified main.pl

--- main.pl
+++ main.pl
@@ -1,4 +1,14 @@
 use strict;
 use warnings;
-
+use Win32::GuiTest qw(FindWindowLike GetWindowText 
+  SetForegroundWindow SendKeys);
+ 
+$Win32::GuiTest::debug = 0; # Set to "1" to enable verbose mode
+ 
+my @windows = FindWindowLike(0, "^Microsoft Excel", "^XLMAIN\$");
+for (@windows) {
+    print "$_>\t'", GetWindowText($_), "'\n";
+    SetForegroundWindow($_);
+    SendKeys("%fn~a{TAB}b{TAB}{BS}{DOWN}");
+}
 
