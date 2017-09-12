7374e4a4ffd70a3b48c61054208aa3d3a066a2e1
glm3531 <glm3531@gmail.com>
2017-09-12 13:50:14 -0600

    Built sub-routines to generate randomized test case input file and expected output file.

--- main.pl
+++ main.pl
@@ -1,60 +1,106 @@
-#create a perl script which slaves autoIT and does the follow
-#open google chrome and check your email, if prompted for username and password enter them automatically
-
-
-
 use strict;
 use warnings;
 use Win32;
 use Win32::API;
 use Win32::OLE;
-my $Au3 = Win32::OLE->new("AutoItX3.Control");
+use Cwd;
+my $Au3 = Win32::OLE->new ("AutoItX3.Control");
 
-my ($fname, $Curr, $fname1, $bbox, $bbox1, $file1, $error, $path, $filex, $filexx, $contentopf, $tocncx, $filess, $ebookmetadata1, $filecc, $bbox99, $snocounter);
-my ($Line, $Variable, $PreMatch, $taglist, $taglist1, $mapfile, $mapbox, $maptext, $find, $replace);
-my ($foottxt)  = ("");
-my $fpath = $ARGV[0];
-my $fpath2 = $ARGV[1];
+my $rootDir = cwd;
+my $testDir = "$rootDir\\Test_Cases";
 
-#Win32::MsgBox("SET MATHTYPE TRANSLATIONS IF NOT!!!", MB_ICONSTOP, "SET MATHTYPE TRANSLATIONS IF NOT");
-opendir (FF, $fpath) or die Win32::MsgBox("Directory not found!!!", MB_ICONSTOP, "DIRECTORY NOT FOUND");
-my @contents3 = grep /(\.eps$)/, readdir FF;
-foreach $file1 ( @contents3 )
-    {
-        #print "$file1 processing\n";
-        $file1=~s/\.eps$//g;
-        open($path, ">$fpath\\$file1\.txt");
-        close($path);
-        $Au3->run ('C:\Program Files\MathType\MathType.exe');
-        $Au3->WinWaitActive('MathType - Untitled 1');
-        $Au3->Sleep (500);
-        $Au3->Send ('^o');
-        $Au3->WinWaitActive('Open');
-        $Au3->Sleep (1000);
-        $Au3->send ("$fpath\\$file1\.eps");
-        $Au3->Sleep (1500);
-        $Au3->Send ('{ENTER}');
-        $Au3->WinWaitActive('[CLASS:EQNWINCLASS]');
-        $Au3->Sleep (1000);
-        $Au3->Send ('^a');
-        $Au3->Sleep (1000);
-        $Au3->Send ('^c');
-        $Au3->Sleep (1000);
-        $Au3->run ('notepad.exe');
-        $Au3->WinWaitActive('Untitled - Notepad');
-        $Au3->Sleep (500);
-        $Au3->Send ('^o');
-        $Au3->WinWaitActive('Open');
-        $Au3->send ("$fpath\\$file1\.txt");
-        $Au3->Sleep (500);
-        $Au3->Send ('{ENTER}');
-        $Au3->WinWaitActive("$file1\.txt");
-        $Au3->Send ('^v');
-        $Au3->Sleep (1500);
-        $Au3->Send ('!fx');
-        $Au3->WinWaitActive('Notepad');
-        $Au3->Send ('!y');
-        $Au3->ProcessClose('mathtype.exe');                
-        $Au3->Sleep (1000);
-        print "$file1 completed\n";
+#######################Generate addition test input file.###############################
+open my $fileHandle, '>',
+    "$testDir\\addition_test_input_file.txt" or die
+        "Cannot create test case file: $testDir\\addition_test_input_file.txt!";
+my $numOfTestCases = 100;
+
+while ($numOfTestCases-- > 0) {
+  my @numbers = genNumbers();
+  addArrayelements($fileHandle,\@numbers);
+}
+close $fileHandle;
+#######################################################################################
+
+
+#######Generate text file of expected outputs from addition_test_input_file.txt########
+open my $_fileHandle, '<',
+    "$testDir\\addition_test_input_file.txt" or die
+        "Cannot create test case file: $testDir\\addition_test_input_file.txt!";
+
+open my $FH, '>', "$testDir\\expected_output.txt" or die
+        "Cannot create test case file: $testDir\\expected_output.txt!";
+
+while (my $line = <$_fileHandle>) {
+chomp $line;
+my @values = split /=/, $line;
+my $n = scalar @values;
+print $FH $values[$n-1];
+print $FH "\n";
+}
+close $FH;
+close $_fileHandle;
+########################################################################################
+
+
+#Write code to slave AutoIT with perl and execute each test case in test case input file.
+
+
+#open calculator, add two numbers together then close after 2 seconds.
+#$Au3->Run("calc.exe");
+#$Au3->WinWaitActive("Calculator");
+#$Au3->WinWaitActive("Calculator");
+#$Au3->Send("{1}"); #1
+#$Au3->Send("{+}"); # +
+#$Au3->Send("{1}"); #1
+#$Au3->Send("{ENTER}");
+#$Au3->Sleep(2000);
+#$Au3->WinClose("Calculator");
+
+
+
+
+
+
+
+####Take each number in an array and add them together.Print to test case file in the form 1 + 2 + 3... = 100.####
+sub addArrayelements {
+  my $fh = shift;
+  my $arr_ref = shift;
+  my @arr = @$arr_ref;
+  my $size = scalar @arr;
+  my $sum = 0;
+
+  for (my $i = 0; $i < $size; $i++) {
+    print $fh $arr[$i];
+    $sum += $arr[$i];
+    if ($i == $size - 1) {
+      print $fh "=";
+    } else {
+      print $fh "+";
     }
+  }
+
+  print $fh "$sum\n";
+}
+###############################################################################################################
+
+
+#sub routine to return an array of randomly generated arguments.
+sub genNumbers {
+  my $random_num = 2 + int(rand(100));
+
+  my @arr = ();
+  my $i = 0;
+
+  while ($i < $random_num) {
+    my $num = int(rand(10000000));
+    $arr[$i] = $num;
+    $i++;
+  }
+
+  return @arr;
+}
+
+
+
